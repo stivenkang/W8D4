c9a274b7670d7cab65e87e7d42aeba1c1112dbb8
Jiong Qi <jiongqipan@gmail.com>
2023-01-05 16:43:20 -0500

    addNumbers done

---
+++ addNumbers.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) {
+        reader.question("Enter a number: \n", function (answer) {
+            sum += parseInt(answer);
+            console.log(`Current Sum: ${sum}`);
+            addNumbers(sum, --numsLeft, completionCallback);
+        })
+    } else {
+        completionCallback(sum);
+    }
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
--- javascript.js
+++ javascript.js
@@ -2,22 +2,26 @@
 
 class Clock {
     constructor() {
-        const date = 
+        const date = new Date();
 
-        this.hours = date.prototype.getHours();
-        this.minutes = date.prototype.getMinutes();
-        this.seconds = date.prototype.getSeconds();
+        this.hours = date.getHours();
+        this.minutes = date.getMinutes();
+        this.seconds = date.getSeconds();
 
-        setInterval(printTime, 1000);
+        this.printTime();
+        setInterval(this._tick.bind(this), 1000);
+
     }
 
     printTime() {
-        let time = `${this.hour} : ${this.minutes} : ${this.seconds}`;
+        let time = `${this.hours} : ${this.minutes} : ${this.seconds}`;
         console.log (time);
     }
 
     _tick() {
+        console.log(this);
         this.seconds++;
+        
         if (this.seconds === 60) {
             this.seconds = 0;
             this.minutes++;
@@ -32,7 +36,7 @@ class Clock {
             this.seconds = 0;
         }
 
-        printTime()
+        this.printTime();
     }
 }
 
